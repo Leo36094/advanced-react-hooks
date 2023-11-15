cfb27d9fa201fefb3fc03b486c9b3578971dcefd
Leo Chuang <chuangl@garena.com>
2023-11-15 18:27:10 +0800

    UPD: Completed exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,21 +3,54 @@
 
 import * as React from 'react'
 
+function countReducer(state, step) {
+  return state + step
+}
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => setCount(step)
+  return <button onClick={increment}>{count}</button>
+}
+function objCountReducer(state, action) {
+  return {...state, ...(typeof action === 'function' ? action(state) : action)}
+}
+function CounterExtra2({initialCount = 0, step = 1}) {
+  const [countState, changeCount] = React.useReducer(objCountReducer, {
+    count: initialCount,
+  })
+  const {count} = countState
+  const increment = () =>
+    changeCount(currState => ({count: currState.count + step}))
+  return <button onClick={increment}>{count}</button>
+}
+function tranditionalReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: state.count + action.step}
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`)
+  }
+}
+function CounterConventionReducer({initialCount = 0, step = 1}) {
+  const [state, dispatch] = React.useReducer(tranditionalReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
 function App() {
-  return <Counter />
+  // return <Counter />
+  // return <CounterExtra2 />
+  return <CounterConventionReducer />
 }
 
 export default App
